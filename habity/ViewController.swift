7bfb6f7147c288cf2913b06a0396607fb8bced92
kuna-anarbay <kuna_anarbay@icloud.com>
2020-10-28 11:40:07 +0600

    Initial Commit

---
+++ habity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  habity
+//
+//  Created by Kuanysh Anarbay on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
